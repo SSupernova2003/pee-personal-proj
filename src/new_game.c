bc4347baf3392a05bf1ed5f5e425fc95472a57d3
SSupernova2003 <113983752+SSupernova2003@users.noreply.github.com>
2024-06-04 21:30:04 +0100

    Add files via upload

--- src/new_game.c
+++ src/new_game.c
@@ -126,7 +126,9 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    //SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+SetWarpDestination(MAP_GROUP(NONE), MAP_NUM(CUSTOM_HUB_1), WARP_ID_NONE, -1, -1);
+
     WarpIntoMap();
 }
 
